0cc17426d26a6510a13781ade7b79313de05f87f
이유정 <yujlee242@gmail.com>
2022-06-30 10:20:40 +0900

    🔨 메뉴 추가, 수정, 삭제 기능 추가

---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+// step1 요구사항 구현을 위한 전략
+// 쪼개서 한 번에 한 개씩 구현할 수 있도록
+/** TODO 메뉴 추가
+ * [x] 메뉴 이름을 입력하고 확인 버튼을 누르면 메뉴가 추가된다.
+ * [x] 메뉴 이름을 입력하고 엔터키 입력으로 추가한다.
+ * [x] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
+ * [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+ * [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+ * [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+ * TODO 메뉴 수정
+ * [x] 메뉴의 수정 버튼을 누르면 메뉴 이름을 수정하는 모달창이 뜬다.
+ * [x] 모달창에서 새 메뉴명을 입력 받고 확인 버튼을 누르면 메뉴가 수정된다.
+
+ * TODO 메뉴 삭제
+ * [x] 메뉴 삭제 버튼을 누르면 confirm 모달창이 뜬다.
+ * [x] 모달창의 확인 버튼을 누르면 메뉴가 삭제된다.
+ * [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+ */
+
+const $ = (seletctor) => document.querySelector(seletctor);
+
+function App() {
+  const updateMenuCount = () => {
+    let menuCount = $('#espresso-menu-list').children.length;
+    $('.menu-count').innerText = `총 ${menuCount++}개`;
+  };
+
+  const addMenuName = () => {
+    if ($('#espresso-menu-name').value === '') {
+      alert('메뉴 이름을 입력해주세요');
+      return;
+    }
+
+    const newMenu = $('#espresso-menu-name').value;
+    const menuItemTemplete = (newMenu) => {
+      return `<li data-menu-id="0" class="menu-list-item d-flex items-center py-2">
+      <span class="w-100 pl-2 menu-name">${newMenu}</span>
+      <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button">품절</button>
+      <button type="button" class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button">수정</button>
+      <button type="button" class="bg-gray-50 text-gray-500 text-sm menu-remove-button">삭제</button>
+    </li>`;
+    };
+
+    $('#espresso-menu-list').insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplete(newMenu)
+    );
+
+    updateMenuCount();
+    $('#espresso-menu-name').value = '';
+  };
+
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+    const updatedMenuName = prompt(
+      '수정할 메뉴명을 입력하세요',
+      $menuName.innerText
+    );
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuNAme = (e) => {
+    if (confirm('정말 삭제하시겠습니까?')) {
+      e.target.closest('li').remove();
+      updateMenuCount(e);
+    }
+  };
+
+  $('#espresso-menu-form').addEventListener('submit', (e) =>
+    e.preventDefault()
+  );
+
+  $('#espresso-menu-submit-button').addEventListener('click', addMenuName);
+
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') addMenuName();
+  });
+
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) updateMenuName(e);
+    if (e.target.classList.contains('menu-remove-button')) removeMenuNAme(e);
+  });
+}
+
+App();
