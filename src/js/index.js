70c495c8880eaa533ba0ad382efa2173c3f96e62
이유정 <yujlee242@gmail.com>
2022-07-05 10:13:00 +0900

    ♻️  nav 이벤트 리스너 내의 함수 코드 분리

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,25 @@ import { $ } from './utils/dom.js';
 // import { store } from './store/store.js';
 import menuApi from './api/menu.js';
 
+/**
+ * 스텝3 요구사항 => 요구하는 단위를 최소한으로 쪼개야 함. 도움 요청시에도 쉽게 요청하고 받을 수 있음
+ * TODO 서버 요청
+ * [x] 웹 서버를 띄운다
+ * [x] 서버에 새로운 메뉴명이 추가될 수 있도록 요청한다.
+ * [x] 서버에 카테고리별 메뉴 리스트를 요청한다.
+ * [x] 서버에 수정된 메뉴명을 업데이트 할 수 있도록 요청한다.
+ * [x] 서버에 품절상태를 토글할 수 있도록 요청한다.
+ * [x] 서버에 메뉴가 삭제 될 수 있도록 요청한다.
+ *
+ * TODO 리팩터링
+ * [x] localStorage에 저장하는 로직은 지운다.
+ * [x] fetch 비동기 api를 사용하는 부분을 async await을 사용하여 구현한다.
+ *
+ * TODO UX
+ * [] API 통신이 실패하는 경우에 대해 사용자가 알 수 있게 alert으로 예외처리를 진행한다.
+ * [] 중복되는 메뉴는 추가할 수 없다.
+ */
+
 function App() {
   this.menu = {
     espresso: [],
@@ -75,6 +94,15 @@ function App() {
     render();
   };
 
+  const changeCategory = (e) => {
+    const isCategoryButton = e.target.classList.contains('cafe-category-name');
+    if (isCategoryButton) {
+      this.currentCategory = e.target.dataset.categoryName;
+      $('#category-title').innerText = `${e.target.innerText} 메뉴 관리`;
+      render();
+    }
+  };
+
   const initEventListeners = () => {
     $('#menu-form').addEventListener('submit', (e) => e.preventDefault());
 
@@ -84,14 +112,7 @@ function App() {
       if (e.key === 'Enter') addMenuName();
     });
 
-    $('nav').addEventListener('click', async (e) => {
-      const isCategoryButton = e.target.classList.contains('cafe-category-name');
-      if (isCategoryButton) {
-        this.currentCategory = e.target.dataset.categoryName;
-        $('#category-title').innerText = `${e.target.innerText} 메뉴 관리`;
-        render();
-      }
-    });
+    $('nav').addEventListener('click', changeCategory);
 
     $('#menu-list').addEventListener('click', (e) => {
       if (e.target.classList.contains('menu-edit-button')) {
